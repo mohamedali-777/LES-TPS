04cfe21dc5fdd59ed66386cadb873a5b321a59e8
dali <mohamedali.chouchanehamila@isimg.tn>
2024-12-04 21:53:12 +0100

    Update main.cpp

--- tp3/EX1/main.cpp
+++ tp3/EX1/main.cpp
@@ -2,22 +2,16 @@
 using namespace std;
 //EX1
 
-// Fonction pour créer un tableau dynamiquement
-
 int* creation(int taille){
     return new int[taille];
 }
 
-// Fonction pour remplir le tableau avec des entiers
-
 void remplir(int* tab, int taille){
-    cout << "Entrez " << taille << " valeurs entières :" << endl;
+    cout << "Entrez " << taille << " valeurs entiÃ¨res :" << endl;
     for (int i = 0; i < taille; ++i) {
         cin >> tab[i];
     }
 }
-
-// Fonction pour compter les valeurs positive
 
 int nbrpos(const int* tab, int taille) {
     int count = 0;
@@ -35,19 +29,11 @@ int main() {
     cout << "Entrez la taille du tableau : ";
     cin >> taille;
 
-    // Allocation dynamique du tableau
     int* array = creation(taille);
-
-    // Remplir le tableau
     remplir(array, taille);
-
-    // Compter les valeurs positives
     int nbrpositive = nbrpos(array, taille);
-
-    // Afficher le résultat
     cout << "Nombre de valeurs positives : " << nbrpositive << endl;
 
-    // Libération de la mémoire
     delete[] array;
 
     return 0;
