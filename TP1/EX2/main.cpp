de7c20cbe2b2f1e6f1da34cad3d2e6707d6e5db4
dali <mohamedali.chouchanehamila@isimg.tn>
2024-12-07 20:36:12 +0100

    Update main.cpp

--- TP1/EX2/main.cpp
+++ TP1/EX2/main.cpp
@@ -7,20 +7,16 @@ int main() {
 
     cout << "Veuillez entrer 10 entiers:\n";
     for (int i = 0; i < 10; i++) {
-        cout << "Entier " << i + 1 << ": ";
+        cout<<"Entier "<<i+1<<": ";
         cin >> entiers[i];
     }
 
     mini=entiers[0];
-
-
     for (int i = 1; i < 10; i++) {
         if (entiers[i] < mini) {
             mini = entiers[i];
         }
     }
-
-
     cout << "Le plus petit entier est: " <<mini<< endl;
 
     return 0;
