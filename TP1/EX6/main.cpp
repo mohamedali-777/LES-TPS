f12c2f10d26ba3b76b62eb4d16c39ed5f690b730
dali <mohamedali.chouchanehamila@isimg.tn>
2024-12-07 20:42:29 +0100

    Update main.cpp

--- TP1/EX6/main.cpp
+++ TP1/EX6/main.cpp
@@ -2,74 +2,51 @@
 using namespace std;
 
 // Fonction swap par valeur
-/*
 void swapv(int a, int b) {
     int temp = a;
     a = b;
     b = temp;
-
-    // Afficher les valeurs après l'échange dans la fonction
-    cout << "Après swapv: a = " << a << ", b = " << b << endl;
+    cout << "AprÃ¨s swapv: a = " << a << ", b = " << b << endl;
 }
 
-int main() {
-    int a,b;
-    cout<<"donner nbr1 :";
-    cin>>a;
-    cout<<"donner nbr2 :";
-    cin>>b;
-    cout << "Avant swapv: a = " << a << ", b = " << b << endl;
-    swapv(a, b);  // Passé par valeur, l'échange n'affecte pas a et b
-    cout << "Après swapv dans main: a = " << a << ", b = " << b << endl;
-    return 0;
-}
-*/
-
-
-// Fonction swap par référence
-/*
+// Fonction swap par rÃ©fÃ©rence
 void swapr(int &a, int &b) {
     int temp = a;
     a = b;
     b = temp;
-
-    // Afficher les valeurs après l'échange dans la fonction
-    cout << "Après swapr: a = " << a << ", b = " << b << endl;
+    cout << "AprÃ¨s swapr: a = " << a << ", b = " << b << endl;
 }
 
-int main() {
-    int a,b;
-    cout<<"donner nbr1 :";
-    cin>>a;
-    cout<<"donner nbr2 :";
-    cin>>b;
-    cout << "Avant swapr: a = " << a << ", b = " << b << endl;
-    swapr(a, b);  // Passé par référence, l'échange affecte a et b
-    cout << "Après swapr dans main: a = " << a << ", b = " << b << endl;
-    return 0;
-}*/
-
-
 // Fonction swap par pointeur
-
 void swapp(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
-
-    // Afficher les valeurs après l'échange dans la fonction
-    cout << "Après swapp: a = " << *a << ", b = " << *b << endl;
+    cout << "AprÃ¨s swapp: a = " << *a << ", b = " << *b << endl;
 }
 
 int main() {
-    int a,b;
-    cout<<"donner nbr1 :";
-    cin>>a;
-    cout<<"donner nbr2 :";
-    cin>>b;
+    int a, b;
+
+    cout << "Donner nbr1 : ";
+    cin >> a;
+    cout << "Donner nbr2 : ";
+    cin >> b;
+
+    cout << "\n=== Swap par valeur ===" << endl;
+    cout << "Avant swapv: a = " << a << ", b = " << b << endl;
+    swapv(a, b);
+    cout << "AprÃ¨s swapv dans main: a = " << a << ", b = " << b << endl;
+
+    cout << "\n=== Swap par rÃ©fÃ©rence ===" << endl;
+    cout << "Avant swapr: a = " << a << ", b = " << b << endl;
+    swapr(a, b);
+    cout << "AprÃ¨s swapr dans main: a = " << a << ", b = " << b << endl;
+
+    cout << "\n=== Swap par pointeur ===" << endl;
     cout << "Avant swapp: a = " << a << ", b = " << b << endl;
-    swapp(&a, &b);  // Passé par pointeur, l'échange affecte a et b
-    cout << "Après swapp dans main: a = " << a << ", b = " << b << endl;
+    swapp(&a, &b);
+    cout << "AprÃ¨s swapp dans main: a = " << a << ", b = " << b << endl;
+
     return 0;
 }
-
