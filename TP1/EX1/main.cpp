f1faf23d05e016db3e0c65de30841606c58b924b
dali <mohamedali.chouchanehamila@isimg.tn>
2024-12-07 20:35:14 +0100

    Update main.cpp

--- TP1/EX1/main.cpp
+++ TP1/EX1/main.cpp
@@ -6,28 +6,23 @@ int main() {
     double distance;
     int choix;
 
-    // Menu de sélection pour l'utilisateur
 
     cout << "Choisissez l'option de conversion:\n";
-    cout << "1. Kilomètres vers Miles\n";
-    cout << "2. Miles vers Kilomètres\n";
+    cout << "1. KilomÃ¨tres vers Miles\n";
+    cout << "2. Miles vers KilomÃ¨tres\n";
     cout << "Entrez votre choix (1 ou 2): ";
     cin >> choix;
 
     if (choix==1) {
-        // Conversion de kilomètres vers miles
-
-        cout << "Entrez la distance en kilomètres: ";
-        cin >> distance;
-        double miles = distance / 1.609;  // Formule de conversion
-        cout <<distance<< " kilomètres = " <<miles<< " miles" << endl;
-    } else if (choix == 2) {
-        // Conversion de miles vers kilomètres
-
+        cout <<"Entrez la distance en kilomÃ¨tres: ";
+        cin>>distance;
+        double miles =distance / 1.609;  
+        cout <<distance<<" kilomÃ¨tres = " <<miles<< " miles" << endl;
+    } else if (choix==2) {
         cout << "Entrez la distance en miles: ";
         cin >> distance;
-        double kilometers = distance * 1.609;  // Formule de conversion
-        cout << distance <<" miles = "<< kilometers <<" kilomètres"<< endl;
+        double kilometers = distance * 1.609;  
+        cout << distance <<" miles = "<< kilometers <<" kilomÃ¨tres"<< endl;
     } else {
         cout << "Choix invalide veuillez entrer 1 ou 2." << endl;
     }
