513bf7defd2589484d0cb30738d58716bf793974
dali <mohamedali.chouchanehamila@isimg.tn>
2024-12-07 20:36:38 +0100

    Update main.cpp

--- TP1/EX3/main.cpp
+++ TP1/EX3/main.cpp
@@ -6,22 +6,16 @@ int main() {
     int n;
     cout << "Combien de mots souhaitez-vous entrer ? ";
     cin >>n;
-
     string mot,pluslong;
     int longmax = 0;
-
     for (int i = 0; i < n; i++) {
         cout << "Entrez le mot " << i + 1 << ": ";
         cin >> mot;
-
-
         if (mot.length() > longmax) {
             pluslong = mot; 
             longmax = mot.length();
         }
     }
-
-
     cout << "Le mot le plus long est: " << pluslong << endl;
 
     return 0;
